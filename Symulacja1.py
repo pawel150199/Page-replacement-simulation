2d4734f07f28569451cbe5f0b58667589b5f05ed
pawel150199 <41188005+pawel150199@users.noreply.github.com>
2021-01-12 22:49:41 +0100

    Add files via upload

--- Symulacja1.py
+++ Symulacja1.py
@@ -155,7 +155,7 @@ class Sym1(object):
         p = input(
             "Podaj dzielnik prędkości przetwarzania procesów(n=1000 wowczas 1cykl=1ms)")
         print()
-        print("------------------------------Algorytm priorytetowy FCFS-------------------------------")
+        print("------------------------------Algorytm  FCFS-------------------------------")
         while flag == True:
             if self.ctime[k] <= t:
                 print()
